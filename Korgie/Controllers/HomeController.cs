1427f64b542cd0cb5c3a6c2221d04ad1f32de528
Anti_Dalai_Lama <princeartik@gmail.com>
2015-10-25 12:08:54 +0300

    Database file created

---
+++ Korgie/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Korgie.Models;
+
+namespace Korgie.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home - it's place for home page! There will be login form and sign up form
+        public ActionResult Index()
+        {
+            return View(); //return main page if user didn't log in and calendar page if he is logged in
+        }
+
+
+    }
+}
