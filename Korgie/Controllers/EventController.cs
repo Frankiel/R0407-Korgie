25d0897b96be93bbad36cac72b70f2b7b9101e7d
Frank Martin <au.frankiel@gmail.com>
2015-11-08 15:51:03 +0200

    US-1: Added get, save methods for events

--- Korgie/Controllers/EventController.cs
+++ Korgie/Controllers/EventController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using Korgie.Models;
+using System.Data.SqlClient;
 
 namespace Korgie.Controllers
 {
@@ -33,31 +34,56 @@ namespace Korgie.Controllers
 
         public string GetEvents(int month, int year)
         {
-            // Get all events for set month and year from DB
-            //using (var conn = new SqlConnection("Server = tcp:ivqgu1eln8.database.windows.net,1433; Database = korgie_db; User ID = frankiel@ivqgu1eln8; Password = Helloworld123; Trusted_Connection = False; Encrypt = True; Connection Timeout = 30"))
-            //{
-            //    //var cmd = new SqlCommand(@"SELECT * WHERE MONTH(Start)=@Monthday AND YEAR(Start)=@Yearday FROM Events", conn);
-            //    conn.Open();
-            //    var cmd = new SqlCommand(@"Select * from AspNetUsers",conn);
-            //    cmd.Parameters.AddWithValue("@Monthday", month);
-            //    cmd.Parameters.AddWithValue("@Yearday", year);
-            //    using (SqlDataReader dr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection))
-            //    {
-            //        for (int i=0;i<dr.FieldCount;i++)
-            //        {
-            //            Console.Write("{0}\t", dr.GetName(i).ToString().Trim());
-            //        }
-            //        while (dr.Read())
-            //        {
-            //                Console.WriteLine("{0}\t{1}\t{2}", dr.GetValue(0).ToString().Trim(),
-            //                dr.GetValue(1).ToString().Trim(),
-            //                dr.GetValue(2).ToString().Trim());
-            //        }
-            //    }
-            //}
-            DateTime now = DateTime.Now;
-            Event[] eventsStub = { new Event(1, "event1", new DateTime(year, month, now.Day, 12, 00, 00), new DateTime(year, month, now.Day, 13, 00, 00), "Sports", "a few words", 0, new List<string>() { "123", "234" }), new Event(2, "event2", new DateTime(year, month, now.Day, 12, 00, 00), new DateTime(year, month, now.Day, 13, 00, 00), "Work", "a few words", 0, new List<string>() { "123", "234" }), new Event(3, "event3", new DateTime(year, month, now.Day, 12, 00, 00), new DateTime(year, month, now.Day, 13, 00, 00), "Study", "a few words", 0, new List<string>() { "123", "234" }), new Event(4, "event4", new DateTime(year, month, now.Day, 12, 00, 00), new DateTime(year, month, now.Day, 13, 00, 00), "Work", "a few words", 0, new List<string>() { "123", "234" }) };
+            Event[] eventsStub = GetEventsUNI(@"SELECT * FROM Events WHERE MONTH(Start)=@Value1 AND YEAR(Start)=@Value2", month, year);
             return new JavaScriptSerializer().Serialize(eventsStub);
+        }
+        public string GetWeekEvent(int week,int year)
+        {
+            Event[] eventsStub = GetEventsUNI(@"SELECT * FROM Events WHERE WEEKOFYEAR(Start)=@Value1 AND YEAR(Start)=@Value2", week, year);
+            return new JavaScriptSerializer().Serialize(eventsStub);
+        }
+        public void SaveEvents(Event _event)
+        {
+            Event[] eventsStub = GetEventsUNI(@"SELECT * FROM Events WHERE EventId=@Value1", _event.EventId);
+            using (var conn = new SqlConnection("Server = tcp:ivqgu1eln8.database.windows.net,1433; Database = korgie_db; User ID = frankiel@ivqgu1eln8; Password = Helloworld123; Trusted_Connection = False; Encrypt = True; Connection Timeout = 30"))
+            {
+                if (eventsStub.Length==0)
+                {
+
+                    string sql = string.Format(@"INSERT INTO Events VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')", _event.EventId, _event.Title, _event.Start, _event.Type, 
+                        _event.Description, _event.Period, _event.Days, _event.Tags);
+                    var cmd = new SqlCommand(sql);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    string sql = string.Format(@"UPDATE Events SET EventId='{0}', Title='{1}', Start='{2}', Type='{3}', Description='{4}', Period='{5}', Days='{6}', Tags='{7}' WHERE EventId='{0}'");
+                    var cmd = new SqlCommand(sql);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        private Event[] GetEventsUNI(string sqlcommand,int value1=0,int value2=0)
+        {
+            //Get all events for set month and year from DB
+            List<Event> events = new List<Event>();
+            using (var conn = new SqlConnection("Server = tcp:ivqgu1eln8.database.windows.net,1433; Database = korgie_db; User ID = frankiel@ivqgu1eln8; Password = Helloworld123; Trusted_Connection = False; Encrypt = True; Connection Timeout = 30"))
+            {
+                var cmd = new SqlCommand(sqlcommand, conn);
+                conn.Open();
+                //var cmd = new SqlCommand(@"Select * from AspNetUsers",conn);
+                cmd.Parameters.AddWithValue("@Value1", value1);
+                cmd.Parameters.AddWithValue("@Value2", value2);
+                using (SqlDataReader dr = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection))
+                {
+                    while (dr.Read())
+                    {
+                        events.Add(new Event(dr.GetInt32(0), dr.GetString(1), dr.GetDateTime(2), dr.GetString(3), dr.GetString(4), dr.GetInt32(5), dr.GetString(7)));
+                    }
+                }
+            }
+            Event[] eventsStub = events.ToArray();
+            return eventsStub;
         }
     }
 }
--- Korgie/Models/Event.cs
+++ Korgie/Models/Event.cs
@@ -10,7 +10,6 @@ namespace Korgie.Models
         public int EventId { get; set; }
         public string Title { get; set; }
         public DateTime Start { get; set; }
-        public DateTime End { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
         //Period of repeating
@@ -19,14 +18,13 @@ namespace Korgie.Models
         //Days of repeating
         public int Days { get; set; } //?????
         //List of tags
-        public List<string> Tags { get; set; }
+        public string Tags { get; set; }
 
-        public Event(int id, string title, DateTime start, DateTime end, string type, string description, int period, /* days ???, */ List<string> tags)
+        public Event(int id, string title, DateTime start, string type, string description, int period, /* days ???, */ string tags)
         {
             EventId = id;
             Title = title;
             Start = start;
-            End = end;
             Type = type;
             Description = description;
             Period = period;
