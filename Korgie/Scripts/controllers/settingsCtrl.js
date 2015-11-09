89282c7f52137e1e7758c1fad3d3007acc4a4de3
Anti_Dalai_Lama <princeartik@gmail.com>
2015-11-09 16:21:47 +0300

    US-18: Adding colorpicker.

--- Korgie/Scripts/controllers/settingsCtrl.js
+++ Korgie/Scripts/controllers/settingsCtrl.js
@@ -5,4 +5,43 @@ korgie.controller('SettingsCtrl', ['$scope', function ($scope) {
         $('.content').toggleClass('opened-menu');
         $('.dark-div').toggleClass('opened-menu');
     }
+
+    $scope.slideWorkColors = function () {
+        $('#workcolors').toggleClass('colors-closed');
+        $('#sportcolors').addClass('colors-closed');
+        $('#relaxingcolors').addClass('colors-closed');
+        $('#studyingcolors').addClass('colors-closed');
+        $('#additionalcolors').addClass('colors-closed');
+    }
+    $scope.slideSportColors = function () {
+        $('#sportcolors').toggleClass('colors-closed');
+        $('#workcolors').addClass('colors-closed');
+        $('#relaxingcolors').addClass('colors-closed');
+        $('#studyingcolors').addClass('colors-closed');
+        $('#additionalcolors').addClass('colors-closed');
+    }
+    $scope.slideRelaxingColors = function () {
+        $('#relaxingcolors').toggleClass('colors-closed');
+        $('#sportcolors').addClass('colors-closed');
+        $('#workcolors').addClass('colors-closed');
+        $('#studyingcolors').addClass('colors-closed');
+        $('#additionalcolors').addClass('colors-closed');
+    }
+    $scope.slideStudyingColors = function () {
+        $('#studyingcolors').toggleClass('colors-closed');
+        $('#sportcolors').addClass('colors-closed');
+        $('#relaxingcolors').addClass('colors-closed');
+        $('#workcolors').addClass('colors-closed');
+        $('#additionalcolors').addClass('colors-closed');
+    }
+    $scope.slideAdditionalColors = function () {
+        $('#additionalcolors').toggleClass('colors-closed');
+        $('#sportcolors').addClass('colors-closed');
+        $('#relaxingcolors').addClass('colors-closed');
+        $('#studyingcolors').addClass('colors-closed');
+        $('#workcolors').addClass('colors-closed');
+    }
+    $scope.selectWorkColor = function (clr) {
+        $('#work').css("background-color", clr);
+    }
 }]);
