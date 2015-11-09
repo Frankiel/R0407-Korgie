e8ac7301c276a976a44dfdfa5e03a29a13fb4f9f
Anti_Dalai_Lama <princeartik@gmail.com>
2015-11-09 14:53:34 +0300

    US-18: Added settings in sections and jumping between pages.

---
+++ Korgie/Scripts/controllers/settingsCtrl.js
@@ -0,0 +1,8 @@
+﻿
+korgie.controller('SettingsCtrl', ['$scope', function ($scope) {
+    $scope.showHideMenu = function () {
+        $('.header').toggleClass('opened-menu');
+        $('.content').toggleClass('opened-menu');
+        $('.dark-div').toggleClass('opened-menu');
+    }
+}]);
