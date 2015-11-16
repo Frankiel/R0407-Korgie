52b68f8535abadc9680c81abecbc1aecb405759b
Anti_Dalai_Lama <princeartik@gmail.com>
2015-11-16 14:00:28 +0300

    US-18: Bugfix of saving profile info after first sign up

--- Korgie/Controllers/AccountController.cs
+++ Korgie/Controllers/AccountController.cs
@@ -177,7 +177,7 @@ namespace Korgie.Controllers
                         Response.Cookies.Add(cookie);
                         using (var conn = new SqlConnection("Server = tcp:ivqgu1eln8.database.windows.net,1433; Database = korgie_db; User ID = frankiel@ivqgu1eln8; Password = Helloworld123; Trusted_Connection = False; Encrypt = True; Connection Timeout = 30"))
                         {
-                            var cmd = new SqlCommand(@"INSERT INTO Users VALUES (@Email,@Name,"","","","","","","","","")", conn);
+                            var cmd = new SqlCommand(@"INSERT INTO Users VALUES (@Email,@Name,'','','','','','','','','')", conn);
                             cmd.Parameters.AddWithValue("@Email", user.Email);
                             cmd.Parameters.AddWithValue("@Name", user.UserName);
                             conn.Open();
