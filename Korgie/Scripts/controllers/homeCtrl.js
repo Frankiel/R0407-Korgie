5b66d1895e9bd4c978b1415fb84dc0fa3ffa573e
EliseevaMaria <maria97.55ua@gmail.com>
2015-11-22 00:54:50 +0200

    Menu hides when item is selected (mobile)

--- Korgie/Scripts/controllers/homeCtrl.js
+++ Korgie/Scripts/controllers/homeCtrl.js
@@ -1,5 +1,4 @@
-﻿
-korgie.controller('HomeCtrl', function ($scope) {
+﻿korgie.controller('HomeCtrl', function ($scope) {
     $scope.openSettings = function () {
         //$('.section-event-page').hide();
         //$('.section-settings-page').show();
@@ -7,7 +6,8 @@ korgie.controller('HomeCtrl', function ($scope) {
         $('#events').addClass("btn--flat");
         $('#settings').removeClass("btn--icon");
         $('#settings').addClass("btn--fab");
-    }
+    };
+
     $scope.openEvents = function () {
         //$('.section-event-page').show();
         //$('.section-settings-page').hide();
@@ -15,5 +15,11 @@ korgie.controller('HomeCtrl', function ($scope) {
         $('#events').addClass("btn--raised");
         $('#settings').removeClass("btn--fab");
         $('#settings').addClass("btn--icon");
-    }
+    };
+
+    $('[ui-sref]').click(function () {
+        $('.header').removeClass('opened-menu');
+        $('.content').removeClass('opened-menu');
+        $('.dark-div').removeClass('opened-menu');
+    });
 });
