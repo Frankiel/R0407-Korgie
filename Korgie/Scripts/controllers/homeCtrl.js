077b3386a942020f49e8bd408f465100b5681c9d
kaonkit <annakuzochkina@gmail.com>
2015-11-30 02:40:55 +0200

    Mobile menu hiding improved

--- Korgie/Scripts/controllers/homeCtrl.js
+++ Korgie/Scripts/controllers/homeCtrl.js
@@ -5,9 +5,9 @@
     //console.log(korgieApi.state);
     setTimeout(function () { console.log($state.current.name);}, 200);
 
-    //$('[ui-sref]').click(function () {
-    //    $('.header').removeClass('opened-menu');
-    //    $('.content').removeClass('opened-menu');
-    //    $('.dark-div').removeClass('opened-menu');
-    //});
+    $('[ui-sref]').click(function () {
+        $('.header').removeClass('opened-menu');
+        $('.content').removeClass('opened-menu');
+        $('.dark-div').removeClass('opened-menu');
+    });
 });
