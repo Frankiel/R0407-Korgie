92084d9607b5f34e62310ed19f4738e2e4293ccb
Frank Martin <au.frankiel@gmail.com>
2015-12-06 21:33:10 +0200

    US-17: no-cache

--- Korgie/Global.asax.cs
+++ Korgie/Global.asax.cs
@@ -14,5 +14,15 @@ namespace Korgie
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new OutputCacheAttribute
+            {
+                VaryByParam = "*",
+                Duration = 0,
+                NoStore = true,
+            });
+            // the rest of your global filters here
+        }
     }
 }
